3808171b8fb991384a87e40cb66a229bdc39c68b
Ricardo da Rocha <ricardodarocha@outlook.com>
2024-11-06 20:02:15 -0300

    migrations itens de pedido e view

--- migrations/20241028061128_view.sql
+++ migrations/20241028061128_view.sql
@@ -5,12 +5,12 @@ CREATE VIEW pedido_view AS
            c.id name,
            c.cidade,
            i.*,
-           p.description,
+           p.descricao,
            p.preco,
            p.preco * i.quant AS total
       FROM pedido o
            JOIN cliente c ON o.cliente = c.ID
-           JOIN cidade R ON R.nome = c.city
+           JOIN cidade R ON R.nome = c.cidade
            JOIN ITEM i ON i.num_pedido = o.num
            JOIN produto P ON P.ID = i.produto
            ;
--- migrations/20241103233748_mock_itens.sql
+++ migrations/20241103233748_mock_itens.sql
@@ -5,51 +5,14 @@ CREATE TABLE IF NOT EXISTS item (
 );
 INSERT
     OR IGNORE INTO item (num_pedido, produto, quant)
-VALUES (
-        (
-            SELECT num
-            from pedido
-            limit 1
-        ), (
-            SELECT id
-            FROM produto
-            WHERE descricao = 'Hot Dog'
-        ),
-        3.0
-    );
-INSERT
-    OR IGNORE INTO item (num_pedido, produto, quant)
-VALUES (
-        (
-            SELECT num
-            from pedido
-            limit 1
-        ), (
-            SELECT id
-            FROM produto
-            WHERE descricao = 'Bauru'
-        ),
-        6.0
-    );
-INSERT
-    OR IGNORE INTO item (num_pedido, produto, quant)
-VALUES (
-        (
-            SELECT num
-            from pedido
-            limit 1
-        ), (
-            SELECT id
-            FROM produto
-            WHERE descricao = 'Wrap de Frango'
-        ),
-        5.0
-    );
--- Atualiza o valor
-UPDATE pedido
-SET valor = (
-        SELECT SUM(i.quant * p.preco)
-        FROM item i
-            JOIN produto p ON i.produto = p.id
-        WHERE i.num_pedido = pedido.num
-    );
+select num,
+    id,
+    0.75 * abs(random() % 80) + 21
+from pedido,
+    (
+        SELECT id
+        FROM produto
+        ORDER BY RANDOM()
+        LIMIT 5
+    )
+order by num;
